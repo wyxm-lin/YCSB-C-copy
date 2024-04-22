d0bfa64d828c416c8dc05d00cb3bc038c697885c
wyxm <3460437682@qq.com>
2024-04-22 09:41:40 +0800

    add ceph_db

--- mycmd.sh
+++ mycmd.sh
@@ -1,5 +1,8 @@
-./ycsbc -db basic -threads 4 -P workloads/myworkload.spec > print.txt
+./ycsbc -db ceph -threads 1 -P workloads/myworkload.spec > print.txt
+
+sudo apt-get install libtbb-dev
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+make
 
 # 原网址
 https://github.com/basicthinker/YCSB-C.git 
